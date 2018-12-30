c89f9a2e9f7334f2b4d16633d5c9aa01ec81922e
Alexander Wall <alexswall2016@gmail.com>
2018-12-29 20:07:34 -0500

    modified encrypt_decrypt.py to get files from cmd args

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,14 +1,22 @@
 import pyAesCrypt
+import sys
 from os.path import getsize
 
-cmd = input("encrypt/decrypt? (e or d): ")
-fname = input("File: ")
-password = input("Password: ")
+
+if len(sys.argv) != 4:
+    print("Usage: python3 encrypt_decrypt.py e/d <filename> <password file>")
+    exit(1)
+
+cmd = sys.argv[1]
+fname = sys.argv[2]
+password = open(sys.argv[3]).readline()
 buffer = getsize(fname)*1024
 
 if cmd == 'e':
     pyAesCrypt.encryptFile(fname,fname+".e",password,buffer)
+    print("File encrypted successfully.")
 elif cmd == 'd':
     pyAesCrypt.decryptFile(fname,fname[:len(fname)-2],password,buffer)
+    print("File decrypted successfully.")
 else:
     print("Invalid command.")
